91666c6d9ae1e16352e7ca047d7c314207a06142
brianquest <brian.andrade.quest@gmail.com>
2024-11-27 15:49:03 -0500

    barra de busqueda implementada

--- src/components/TableAdd.tsx
+++ src/components/TableAdd.tsx
@@ -33,7 +33,7 @@ const TableAdd: React.FC<TableAddPros> = ({ onAddComplete }) => {
         <div>
             {/* Botón para abrir el modal de agregar */}
             <button
-                className="px-4 py-2 bg-green-500 text-white rounded hover:bg-blue-600"
+                className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
                 onClick={() => { setAddModalOpen(true) }}
             >
                 Agregar
--- src/components/TableInfo.tsx
+++ src/components/TableInfo.tsx
@@ -1,30 +1,57 @@
 "use client"
-import React, { useEffect, useState } from "react";
+import React, { InputHTMLAttributes, useEffect, useState } from "react";
 import { getSpeciesList, SpecieGetDTO } from "../app/animals/api/specie_api"
 import TableActions from "./TableActions"; //agregar y modificar
 import TableAdd from "@/components/TableAdd";
 
 
+
 const TableInfo = () => {
-    const [species, setSpecies] = useState<SpecieGetDTO[]>([]);// Data de las especies
+    const [species, setSpecies] = useState<SpecieGetDTO[]>([]);// Data  original de especies
     const [loading, setLoading] = useState(true); // Indicar si los datos están cargando
-    const [error, Seterror] = useState<String | null>(null);
+
+    // insumos para la barra search
+    const [filteredSpecies, setFilteredSpecies] = useState<SpecieGetDTO[]>([]); //lista de especies filtrada
+    const [searchTerm, setSearchTerm] = useState("");
 
     // Obtener datos del endpoint
     const fetchSpecies = async () => {
+        setLoading(true);
         try {
             const response = await getSpeciesList(); // Llamo a la API
             if (response.status) {
                 setSpecies(response.data); // Actualizamos el estado con las especies
+                setFilteredSpecies(response.data);
             } else {
-                Seterror(response.messege || "Error del backend"); // Mostramos el error
+                console.log(response.messege || "Error del backend"); // Mostramos el error
             }
         } catch (err) {
-            Seterror("Error del Frontend:" + err);
+            console.log("Error del Frontend:" + err);
         } finally {
             setLoading(false) // Indicamos que terminó la carga
         }
     };
+
+    // Función para manejar el cambio en la barra de búsqueda
+    const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const term = e.target.value.toLowerCase();
+        setSearchTerm(term);
+
+        //filtrar registros
+        const filtered = species.filter((specie) => {
+            // Verificar coincidencias en los campos deseados
+            const groupMatch = specie.nameGroup.toLocaleLowerCase().includes(term);
+            const descriptionMatch = specie.detail.toLocaleLowerCase().includes(term);
+            const animalsMatch = specie.simpleAnimalDTOs
+                .map((animal) => animal.animalName.toLowerCase())
+                .join(", ")
+                .includes(term);
+            return groupMatch || descriptionMatch || animalsMatch;
+
+        });
+        setFilteredSpecies(filtered);
+    };
+
     // Obtener datos del endpoint
     useEffect(() => {
         fetchSpecies() // Ejecutamos la función
@@ -35,7 +62,16 @@ const TableInfo = () => {
     return (
         <div className="overflow-x-auto">
             {/* Barra de herramientas */}
-            <TableAdd onAddComplete={fetchSpecies} />
+            <div className="flex justify-between items-center mb-4">
+                <input
+                    type="text"
+                    placeholder="Buscar por grupo, descripción o animales..."
+                    value={searchTerm}
+                    onChange={handleSearch}
+                    className="border p-2 w-2/3"
+                />
+                <TableAdd onAddComplete={fetchSpecies} />
+            </div>
             <table className="min-w-full border-collapse border border-gray-200">
                 <thead className="bg-gray-100">
                     <tr>
@@ -47,7 +83,7 @@ const TableInfo = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {species.map((specie, index) => (
+                    {filteredSpecies.map((specie, index) => (
                         <tr key={specie.specieId} className="odd:bg-white even:bg-gray-50">
                             <td className="border border-gray-300 px-4 py-2">{index + 1}</td>
                             <td className="border border-gray-300 px-4 py-2">{specie.nameGroup}</td>
